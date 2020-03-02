73d263f50cfe5c7633cb4645720ac6bb637f0e82
danilomacb <danilo.bakun@gmail.com>
2020-03-02 11:00:07 -0300

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/test">test</a></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'NavBar' 
+}
+</script>
+
+<style scoped>
+    ul {
+        width: 100%;
+        background-color: black;
+        color: white;
+    }
+    li {
+        display: inline-block;
+        padding: 10px;
+    }
+    a {
+        color: white;
+        text-decoration: none;
+    }
+</style>
